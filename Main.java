44cc0fa98b9e9e3579dce6032b307f6b02fb10b5
AhmetAcarer <20119046@student.hhs.nl>
2022-03-17 22:23:36 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void genereer(){
+        Random rand = new Random();
+        int[][] temp = new int[250][250];
+        for (int i = 0; i < 250; i++) {
+            for (int j = 0; j < 250; j++) {
+                if (i==j){
+                    temp[i][j]=0;
+                }
+                else if (i>j){
+                    temp[i][j] = temp[j][i];
+                }
+                else{
+                    temp[i][j] = rand.nextInt(100)+1;
+                }
+            }
+        }
+
+        for (int i = 0; i < 250; i++) {
+            for (int j = 0; j < 250; j++) {
+                    System.out.printf("%3d ",temp[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        RouteCalc alg = new RouteCalc();
+        alg.readSituation("invoerbestanden/1.txt");
+        System.out.println("test");
+    }
+}
---
+++ RouteCalc.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.TooManyListenersException;
+
+public class RouteCalc {
+
+    final int TOTALDEST = 250;
+
+    private int[] destinations;
+    private int[] packages;
+    private int[][] distances;
+
+    public RouteCalc(){
+
+    }
+
+    public void readSituation(String file){
+        File situationfile = new File (file);
+        Scanner scan = null;
+        try {
+            scan = new Scanner(situationfile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int size = scan.nextInt ();
+        destinations = new int[size];
+        packages = new int[size];
+        distances = new int[TOTALDEST][TOTALDEST];
+
+        for (int i = 0; i < size; i++) {
+            destinations[i] = scan.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            packages[i] = scan.nextInt();
+        }
+        for (int i = 0; i < TOTALDEST; i++) {
+            for (int j = 0; j < TOTALDEST; j++) {
+                distances[i][j] = scan.nextInt();
+            }
+        }
+    }
+}
